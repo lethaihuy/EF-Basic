599e16ace3b548db26e7b6fc73af8b700d3a9f45
HUY-LE\HUY-LE <lethaihuy77@gmail.com>
2019-03-17 23:24:27 +0700

    Up source

---
+++ EFGUI/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace EFGUI
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtNoiSinh = new System.Windows.Forms.TextBox();
+            this.cbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.cbMaKhoa = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(3, 3);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(586, 387);
+            this.dgvData.TabIndex = 0;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(595, 3);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(620, 282);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(620, 359);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(620, 323);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(669, 57);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(136, 20);
+            this.txtMaSV.TabIndex = 5;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(669, 83);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(136, 20);
+            this.txtHoTen.TabIndex = 6;
+            // 
+            // txtNoiSinh
+            // 
+            this.txtNoiSinh.Location = new System.Drawing.Point(669, 161);
+            this.txtNoiSinh.Name = "txtNoiSinh";
+            this.txtNoiSinh.Size = new System.Drawing.Size(136, 20);
+            this.txtNoiSinh.TabIndex = 9;
+            // 
+            // cbGioiTinh
+            // 
+            this.cbGioiTinh.AutoCompleteCustomSource.AddRange(new string[] {
+            "Nam",
+            "Nữ"});
+            this.cbGioiTinh.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbGioiTinh.FormattingEnabled = true;
+            this.cbGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.cbGioiTinh.Location = new System.Drawing.Point(669, 110);
+            this.cbGioiTinh.Name = "cbGioiTinh";
+            this.cbGioiTinh.Size = new System.Drawing.Size(136, 21);
+            this.cbGioiTinh.TabIndex = 10;
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(669, 138);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(136, 20);
+            this.dtpNgaySinh.TabIndex = 11;
+            // 
+            // cbMaKhoa
+            // 
+            this.cbMaKhoa.FormattingEnabled = true;
+            this.cbMaKhoa.Location = new System.Drawing.Point(669, 187);
+            this.cbMaKhoa.Name = "cbMaKhoa";
+            this.cbMaKhoa.Size = new System.Drawing.Size(136, 21);
+            this.cbMaKhoa.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "MaSV:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(602, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ngày sinh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(602, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Giới tính:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(602, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Họ và tên:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(602, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Nơi sinh:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(602, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Mã khoa:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(724, 243);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 19;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(620, 243);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(811, 394);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbMaKhoa);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.cbGioiTinh);
+            this.Controls.Add(this.txtNoiSinh);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvData);
+            this.Name = "formMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtNoiSinh;
+        private System.Windows.Forms.ComboBox cbGioiTinh;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.ComboBox cbMaKhoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ EFGUI/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFGUI
+{
+    public partial class formMain : Form
+    {
+        SinhVienEntities db = new SinhVienEntities();
+        List<Khoa> listKhoa = new List<Khoa>();
+        string isCheck = "";
+        public formMain()
+        {
+            InitializeComponent();
+            Load();       
+            //AddBinding();
+            LoadListKhoa();
+            Khoa();
+        }
+
+        //fun
+        void LoadListKhoa()
+        {
+            foreach (var item in db.Khoas.Where(k => k.MaKhoa != null).ToList())
+            {
+                listKhoa.Add(item);
+            }
+            cbMaKhoa.DataSource = listKhoa;
+            cbMaKhoa.DisplayMember = "MaKhoa";
+        }
+
+        void Load()
+        {
+            var data = from sv in db.DMSinhViens select new { MaSV = sv.MaSV, HoTen = sv.HoTenSV, GioiTinh = sv.Phai, NgaySinh = sv.NgaySinh, NoiSinh = sv.NoiSinh, MaKhoa = sv.Khoa.MaKhoa };
+            dgvData.DataSource = data.ToList();
+
+            this.dgvData.Columns[0].HeaderText = "Mã SV";
+            this.dgvData.Columns[1].HeaderText = "Họ và tên";
+            this.dgvData.Columns[2].HeaderText = "Giới tính ";
+            this.dgvData.Columns[3].HeaderText = "Ngày Sinh";
+            this.dgvData.Columns[4].HeaderText = "Nơi sinh";
+            this.dgvData.Columns[5].HeaderText = "Mã khoa";
+        }
+
+        void AddBinding()
+        {
+            txtMaSV.DataBindings.Add(new Binding("Text", dgvData.DataSource, "MaSV"));
+            txtHoTen.DataBindings.Add(new Binding("Text", dgvData.DataSource, "HoTenSV"));
+            cbGioiTinh.DataBindings.Add(new Binding("text", dgvData.DataSource, "GioiTinh", true, DataSourceUpdateMode.Never));
+            dtpNgaySinh.DataBindings.Add(new Binding("Text", dgvData.DataSource, "NgaySinh"));
+            txtNoiSinh.DataBindings.Add(new Binding("Text", dgvData.DataSource, "NoiSinh"));
+            cbMaKhoa.DataBindings.Add(new Binding("Text", dgvData.DataSource, "MaKhoa"));
+
+        }
+
+        void AddSV()
+        {
+            try
+            {
+                DMSinhVien sv = new DMSinhVien();
+                sv.MaSV = txtMaSV.Text;
+                sv.HoTenSV = txtHoTen.Text;
+                sv.Phai = cbGioiTinh.Text;
+                sv.NgaySinh = dtpNgaySinh.Value;
+                sv.NoiSinh = txtNoiSinh.Text;
+                sv.MaKhoa = cbMaKhoa.Text;
+
+
+                db.DMSinhViens.Add(sv);
+                db.SaveChanges();
+                Load();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        void DeleteSV()
+        {
+            string maSV = this.dgvData.CurrentRow.Cells[0].Value.ToString();
+            DMSinhVien sv = db.DMSinhViens.Single(v => v.MaSV.Equals(maSV));
+            db.DMSinhViens.Remove(sv);
+            db.SaveChanges();
+        }
+
+        void EditSV()
+        {          
+            string maSV = this.dgvData.SelectedCells[0].OwningRow.Cells["MaSV"].Value.ToString();
+            DMSinhVien sv = db.DMSinhViens.Find(maSV);
+            sv.HoTenSV = txtHoTen.Text;
+            sv.Phai = cbGioiTinh.Text;
+            sv.NgaySinh = dtpNgaySinh.Value;
+            sv.NoiSinh = txtNoiSinh.Text;
+            sv.MaKhoa = cbMaKhoa.Text;
+            db.SaveChanges();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Load();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.isCheck = "save";
+            MoKhoa();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string maSV = this.dgvData.SelectedCells[0].OwningRow.Cells["MaSV"].Value.ToString();
+            if (MessageBox.Show("Bạn có muốn xóa "+maSV+" ?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                DeleteSV();
+                Load();
+            }
+          
+        }
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Khoa();
+            string maSV = this.dgvData.CurrentRow.Cells[0].Value.ToString();
+            DMSinhVien sv = db.DMSinhViens.Single(v => v.MaSV.Equals(maSV));
+            txtMaSV.Text = sv.MaSV;
+            txtHoTen.Text = sv.HoTenSV;
+            cbGioiTinh.Text = sv.Phai;
+            dtpNgaySinh.Value = sv.NgaySinh;
+            txtNoiSinh.Text = sv.NoiSinh;
+            cbMaKhoa.Text = sv.MaKhoa;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.isCheck = "edit";
+            MoKhoa();
+        }
+
+        void Khoa()
+        {
+            txtMaSV.ReadOnly = false;
+            txtHoTen.ReadOnly = true;
+            cbGioiTinh.Enabled = false;
+            dtpNgaySinh.Enabled = false;
+            txtNoiSinh.ReadOnly = true;
+            cbMaKhoa.Enabled = false;
+        }
+
+        void MoKhoa()
+        {
+            btnSave.Enabled = true;
+
+            txtMaSV.ReadOnly = true;
+            txtHoTen.ReadOnly = false;
+            cbGioiTinh.Enabled = true;
+            dtpNgaySinh.Enabled = true;
+            txtNoiSinh.ReadOnly = false;
+            cbMaKhoa.Enabled = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Khoa();
+            txtMaSV.ResetText();
+            txtHoTen.ResetText();
+            cbGioiTinh.ResetText();
+            dtpNgaySinh.ResetText();
+            txtNoiSinh.ResetText();
+            cbMaKhoa.ResetText();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(isCheck == "save")
+            {
+                this.AddSV();
+            }
+            if(isCheck == "edit")
+            {
+                this.EditSV();
+            }
+            this.Load();
+            Khoa();
+        }
+    }
+}
